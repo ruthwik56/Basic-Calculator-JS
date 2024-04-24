1f9f2391f2db517d361e7ec898a15a77733a4160
ruthwik56 <sairuthwik1119@gmail.com>
2024-04-24 09:44:45 +0530

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let screen = document.getElementById("display")
+
+function appendToDisplay(input){
+    screen.value+=input
+}
+function clearDisplay(){
+    screen.value=""
+}
+function calculate(){
+    try{
+        screen.value=eval(screen.value)
+    }
+    catch{
+        screen.value="Error"
+    }
+}
